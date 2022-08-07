4f611d9be19d0a3cd3a4edf653de9c4e29b3c561
Jitendra <jeetscmaker@gmail.com>
2022-08-07 12:02:58 +0530

    code refactored using Java 8 streams.

--- src/main/java/com/jk/StudentService.java
+++ src/main/java/com/jk/StudentService.java
@@ -3,21 +3,17 @@ package com.jk;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
-	@Autowired
-	StudentDao stDao;
-	
-	public List<Student> getFullList() {
-		List<Student> list = new ArrayList<>();
-		Map<Integer, Student> map = stDao.getFullList();
-		for(Student s: map.values()){
-			list.add(s);
-		}
-		return list;
-	}
+    @Autowired
+    StudentDao stDao;
+
+    public List<Student> getFullList() {
+        return stDao.getFullList().values().stream().collect(Collectors.toList());
+    }
 }
